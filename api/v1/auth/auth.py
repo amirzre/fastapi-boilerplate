eba43793f314d31c81d6e6286a4979eb78c5c796
Amir Zare <amirzare309@yahoo.com>
2024-10-25 05:30:38 -0400

    feat: add me or user info endpoint

--- api/v1/auth/auth.py
+++ api/v1/auth/auth.py
@@ -2,9 +2,16 @@ from fastapi import APIRouter, Depends, Request, Response, status
 from redis.asyncio import client
 
 from app.controllers import AuthController
+from app.models import User
 from app.schemas.request import UserLoginRequest
+from app.schemas.response import UserResponse
 from core.factory import Factory
-from core.fastapi.dependencies import get_cache, get_current_user_with_refresh_token
+from core.fastapi.dependencies import (
+    get_authenticated_user,
+    get_cache,
+    get_current_user,
+    get_current_user_with_refresh_token,
+)
 from core.security import JWTHandler
 
 auth_router = APIRouter()
@@ -79,3 +86,11 @@ async def refresh_token(
         expires=JWTHandler.token_expiration(tokens.refresh_token),
     )
     response.headers["X-CSRF-TOKEN"] = tokens.csrf_token
+
+
+@auth_router.get("/me", status_code=status.HTTP_200_OK)
+async def me(user: User = Depends(get_current_user)) -> UserResponse:
+    """
+    Retrieve current user information.
+    """
+    return user
