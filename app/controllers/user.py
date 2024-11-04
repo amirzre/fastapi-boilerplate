473d71b92d4d4826154d08722035e864b74c1126
Amir Zare <amirzare309@yahoo.com>
2024-11-04 11:35:59 +0330

    feat: change response model to get users with paginate info

--- app/controllers/user.py
+++ app/controllers/user.py
@@ -2,8 +2,9 @@ from uuid import UUID
 
 from app.models import User
 from app.repositories import UserRepository
-from app.schemas.extra import UserFilterParams
+from app.schemas.extra import PaginationResponse, UserFilterParams
 from app.schemas.request import RegisterUserRequest, UpdateUserRequest
+from app.schemas.response import UserResponse
 from core.controller import BaseController
 from core.db import Transactional
 from core.exceptions import BadRequestException, NotFoundException
@@ -19,8 +20,11 @@ class UserController(BaseController[User]):
         super().__init__(model=User, repository=user_repository)
         self.user_repository = user_repository
 
-    async def get_filtered_user(self, *, filter_params: UserFilterParams) -> list[User] | None:
-        return await self.user_repository.get_filtered_users(filter_params=filter_params)
+    async def get_filtered_user(self, *, filter_params: UserFilterParams) -> PaginationResponse[UserResponse]:
+        users, total = await self.user_repository.get_filtered_users(filter_params=filter_params)
+        return PaginationResponse[UserResponse](
+            limit=filter_params.limit, offset=filter_params.offset, total=total, items=users
+        )
 
     async def get_user(self, *, user_uuid: UUID) -> User:
         user = await self.user_repository.get_by_uuid(uuid=user_uuid)
