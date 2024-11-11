a7ae58de4ee559e68611821da87edfa978ee1a78
Amir Zare <amirzare309@yahoo.com>
2024-11-11 15:36:24 +0330

    fix: change cache to accpet pydantic object

--- core/cache/redis_backend.py
+++ core/cache/redis_backend.py
@@ -1,30 +1,44 @@
 import pickle
 from typing import Any
+from uuid import UUID
 
 import orjson
-import redis.asyncio as aioredis
+from pydantic import BaseModel
 
 from core.cache.base import BaseBackend
-from core.config import config
-
-redis = aioredis.from_url(url=config.REDIS_URL)
+from core.redis import redis_client as redis
 
 
 class RedisBackend(BaseBackend):
-    async def get(self, key: str) -> Any:
+    async def get(self, key: str, model: BaseModel = None) -> Any:
         result = await redis.get(key)
         if not result:
             return
 
         try:
-            return orjson.loads(result.decode("utf8"))
+            data = orjson.loads(result.encode("utf8"))
+            return model(**data) if model else data
         except UnicodeDecodeError:
             return pickle.loads(result)
 
     async def set(self, response: Any, key: str, ttl: int = 60) -> None:
-        if isinstance(response, dict):
-            response = orjson.dumps(response)
-        elif isinstance(response, object):
+        if isinstance(response, BaseModel):
+            data = response.model_dump()
+
+            def convert_uuids(obj):
+                if isinstance(obj, UUID):
+                    return str(obj)
+                elif isinstance(obj, dict):
+                    return {k: convert_uuids(v) for k, v in obj.items()}
+                elif isinstance(obj, list):
+                    return [convert_uuids(i) for i in obj]
+                return obj
+
+            data = convert_uuids(data)
+            response = orjson.dumps(data)
+        elif isinstance(response, dict):
+            response = pickle.dumps(response)
+        else:
             response = pickle.dumps(response)
 
         await redis.set(name=key, value=response, ex=ttl)
