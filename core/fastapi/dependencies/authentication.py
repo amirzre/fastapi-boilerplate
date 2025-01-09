5b64e0848c80c1f30526ca08bd7ef3ee6b4e912d
Amir Zare <amirzare309@yahoo.com>
2025-01-09 01:39:14 -0500

    feat: add translation to authentication deps exceptions

--- core/fastapi/dependencies/authentication.py
+++ core/fastapi/dependencies/authentication.py
@@ -2,6 +2,7 @@ from fastapi import Request
 from fastapi.security import HTTPAuthorizationCredentials
 
 from core.exceptions import UnauthorizedException
+from core.i18n import translate as _
 from core.security import JWTHandler
 
 
@@ -12,19 +13,19 @@ class AuthenticationHandler:
     async def _get_token(self, token_type: str) -> str:
         token = self.request.cookies.get(f"{token_type}-Token")
         if not token:
-            raise UnauthorizedException(message=f"{token_type}-Token is not provided.")
+            raise UnauthorizedException(message=_(f"{token_type}-Token is not provided."))
         return token
 
     async def _decode_token(self, token: str, key: str) -> str:
         decoded_token = JWTHandler.decode(token=token)
         user_uuid = decoded_token.get(key)
         if not user_uuid:
-            raise UnauthorizedException(message="Invalid token.")
+            raise UnauthorizedException(message=_("Invalid token."))
         return user_uuid
 
     async def _validate_token(self, token: str, credentials: HTTPAuthorizationCredentials, token_type: str) -> None:
         if credentials.scheme != "Bearer" or credentials.credentials != token:
-            raise UnauthorizedException(message="Invalid token.")
+            raise UnauthorizedException(message=_("Invalid token."))
 
     async def authenticate_user(
         self, token_type: str, key: str, credentials: HTTPAuthorizationCredentials = None
