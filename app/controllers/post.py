6fbdab8a69fa8962aa8776b7a88ca98c368c302d
Amir Zare <amirzare309@yahoo.com>
2025-01-16 12:53:43 -0500

    feat: add post controller

---
+++ app/controllers/post.py
@@ -0,0 +1,8 @@
+from app.models import Post
+from core.controller import BaseController
+
+
+class PostController(BaseController[Post]):
+    """Post controller provides all the logic operations for the Post model."""
+
+    ...
