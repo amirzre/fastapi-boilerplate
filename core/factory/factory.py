804e14b857497ca48bcda03e20cb96ebf7626b59
Amir Zare <amirzare309@yahoo.com>
2025-01-18 06:42:14 -0500

    feat: add post factory

--- core/factory/factory.py
+++ core/factory/factory.py
@@ -2,9 +2,9 @@ from functools import partial
 
 from fastapi import Depends
 
-from app.controllers import AuthController, UserController
-from app.models import User
-from app.repositories import UserRepository
+from app.controllers import AuthController, PostController, UserController
+from app.models import Post, User
+from app.repositories import PostRepository, UserRepository
 from core.db import get_session
 
 
@@ -15,9 +15,13 @@ class Factory:
     """
 
     user_repository = partial(UserRepository, User)
+    post_repository = partial(PostRepository, Post)
 
     def get_user_controller(self, db_session=Depends(get_session)):
         return UserController(user_repository=self.user_repository(db_session=db_session))
 
     def get_auth_controller(self, db_session=Depends(get_session)):
         return AuthController(user_repository=self.user_repository(db_session=db_session))
+
+    def get_post_controller(self, db_session=Depends(get_session)):
+        return PostController(post_repository=self.post_repository(db_session=db_session))
