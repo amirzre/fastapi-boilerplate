02b15698810eed7cb85bf170bf0008009b8870d7
Amir Zare <amirzare309@yahoo.com>
2025-01-18 06:46:00 -0500

    fix: remove content field from post response

--- app/schemas/response/post.py
+++ app/schemas/response/post.py
@@ -6,7 +6,6 @@ from app.models import PostStatus
 class PostResponse(BaseModel):
     uuid: UUID4 = Field(examples=["a3b8f042-1e16-4f0a-a8f0-421e16df0a2f"])
     title: str = Field(examples=["First Post"])
-    content: str = Field(examples=["Test post content"])
     status: PostStatus = Field(examples=[PostStatus.DRAFT])
     user_id: UUID4 = Field(examples=["a3b8f042-1e16-4f0a-a8f0-421e16df0a2f"])
 
