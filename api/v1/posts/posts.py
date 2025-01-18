3262cc8a074f93dc70885579132dab4e0c21cabd
Amir Zare <amirzare309@yahoo.com>
2025-01-18 08:07:28 -0500

    feat: add update and delete post endpoints

--- api/v1/posts/posts.py
+++ api/v1/posts/posts.py
@@ -3,6 +3,7 @@ from pydantic import UUID4
 
 from app.controllers import PostController
 from app.schemas.request import CreatePostRequest
+from app.schemas.request.post import UpdatePostRequest
 from app.schemas.response import PostResponse
 from core.factory import Factory
 from core.responses import APIResponse, APIResponseType
@@ -16,7 +17,7 @@ async def get_post(
     post_controller: PostController = Depends(Factory().get_post_controller),
 ) -> APIResponseType[PostResponse]:
     """
-    Get Post by UUID.
+    Retrieve Post by UUID.
     """
     post = await post_controller.get_post(post_uuid=post_id)
     return APIResponse(post)
@@ -32,3 +33,27 @@ async def create_post(
     """
     post = await post_controller.create_post(create_post_request=create_post_request)
     return APIResponse(post)
+
+
+@post_router.put("/{post_id}", status_code=status.HTTP_200_OK)
+async def update_post(
+    post_id: UUID4,
+    update_post_request: UpdatePostRequest,
+    post_controller: PostController = Depends(Factory().get_post_controller),
+) -> APIResponseType[PostResponse]:
+    """
+    Update a post.
+    """
+    post = await post_controller.update_post(post_uuid=post_id, update_post_request=update_post_request)
+    return APIResponse(post)
+
+
+@post_router.delete("/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(
+    post_id: UUID4,
+    post_controller: PostController = Depends(Factory().get_post_controller),
+) -> None:
+    """
+    Delete a post.
+    """
+    return await post_controller.delete_post(post_uuid=post_id)
