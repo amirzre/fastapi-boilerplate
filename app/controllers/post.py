9ed9c1e10102e7f9a7e105bc5ceca5ef3590f85d
Amir Zare <amirzare309@yahoo.com>
2025-01-18 07:05:51 -0500

    feat: add get post controller

--- app/controllers/post.py
+++ app/controllers/post.py
@@ -1,3 +1,5 @@
+from pydantic import UUID4
+
 from app.models import Post
 from app.repositories import PostRepository
 from app.schemas.request import CreatePostRequest
@@ -14,6 +16,18 @@ class PostController(BaseController[Post]):
     def __init__(self, post_repository: PostRepository):
         super().__init__(model=Post, repository=post_repository)
         self.post_repository = post_repository
+
+    async def get_post(self, *, post_uuid: UUID4) -> PostResponse:
+        post = await self.post_repository.get_by_uuid(uuid=post_uuid)
+        if not post:
+            raise NotFoundException(message=_("Post not found."))
+
+        return PostResponse(
+            uuid=post.uuid,
+            title=post.title,
+            status=post.status,
+            user_id=post.user_id,
+        )
 
     @Transactional()
     async def create_post(self, *, create_post_request: CreatePostRequest) -> PostResponse:
