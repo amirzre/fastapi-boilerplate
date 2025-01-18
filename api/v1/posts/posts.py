2c51e910ac249f32393010709b2f2ec371399993
Amir Zare <amirzare309@yahoo.com>
2025-01-18 07:06:07 -0500

    feat: add get post endpoint

--- api/v1/posts/posts.py
+++ api/v1/posts/posts.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, status
+from pydantic import UUID4
 
 from app.controllers import PostController
 from app.schemas.request import CreatePostRequest
@@ -7,6 +8,18 @@ from core.factory import Factory
 from core.responses import APIResponse, APIResponseType
 
 post_router = APIRouter()
+
+
+@post_router.get("/{post_id}", status_code=status.HTTP_200_OK)
+async def get_post(
+    post_id: UUID4,
+    post_controller: PostController = Depends(Factory().get_post_controller),
+) -> APIResponseType[PostResponse]:
+    """
+    Get Post by UUID.
+    """
+    post = await post_controller.get_post(post_uuid=post_id)
+    return APIResponse(post)
 
 
 @post_router.post("/", status_code=status.HTTP_201_CREATED)
