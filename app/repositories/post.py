a5403c8b2528feb7bc2a5aaef03d5a0be1997f2c
Amir Zare <amirzare309@yahoo.com>
2025-01-18 06:46:57 -0500

    feat: add get user by uuid method

--- app/repositories/post.py
+++ app/repositories/post.py
@@ -1,8 +1,20 @@
-from app.models import Post
+from pydantic import UUID4
+from sqlalchemy import select
+
+from app.models import Post, User
 from core.repository import BaseRepository
 
 
 class PostRepository(BaseRepository[Post]):
     """Post repository provides all the database operations for the Post model."""
 
-    ...
+    async def get_user_by_uuid(self, uuid: UUID4) -> User | None:
+        """
+        Get User by UUID.
+
+        param uuid: User UUID.
+        return : User or None.
+        """
+        query = select(User).filter(User.uuid == uuid)
+
+        return await self._one_or_none(query)
