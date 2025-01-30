bb6186676fc51ea4a592cea30fcd6f2f71e20c52
Amir Zare <amirzare309@yahoo.com>
2025-01-30 09:04:05 -0500

    feat: add access control to models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,6 @@
 from core.db import Base
 
-from .post import Post, PostStatus
-from .user import User, UserRole
+from .post import Post, PostPermission, PostStatus
+from .user import User, UserPermission, UserRole
 
-__all__ = ["Base", "User", "UserRole", "Post", "PostStatus"]
+__all__ = ["Base", "User", "UserRole", "Post", "PostStatus", "UserPermission", "PostPermission"]
