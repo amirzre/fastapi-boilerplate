09d3b291c7c60f24da7e6770e358d04ca205d235
Amir Zare <amirzare309@yahoo.com>
2025-01-30 09:02:02 -0500

    feat: add access control level module

--- core/security/__init__.py
+++ core/security/__init__.py
@@ -1,4 +1,29 @@
+from .access_control import (
+    AccessControl,
+    ACLRegistry,
+    ActionPrincipal,
+    Allow,
+    AllowAll,
+    Authenticated,
+    Everyone,
+    PostPrincipal,
+    RolePrincipal,
+    UserPrincipal,
+)
 from .jwt import JWTHandler
 from .password import PasswordHandler
 
-__all__ = ["PasswordHandler", "JWTHandler"]
+__all__ = [
+    "PasswordHandler",
+    "JWTHandler",
+    "AccessControl",
+    "ACLRegistry",
+    "ActionPrincipal",
+    "Allow",
+    "AllowAll",
+    "Authenticated",
+    "Everyone",
+    "PostPrincipal",
+    "RolePrincipal",
+    "UserPrincipal",
+]
