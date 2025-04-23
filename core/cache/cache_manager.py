f03ce83858fcc471cbfabe469adf5708d4594a94
Amir Zare <amirzare309@yahoo.com>
2025-04-23 16:19:05 +0330

    fix: update doc string for cache module.

--- core/cache/cache_manager.py
+++ core/cache/cache_manager.py
@@ -8,11 +8,26 @@ F = TypeVar("F", bound=Callable[..., Any])
 
 
 class CacheManager:
+    """
+    Manages caching operations including setting, retrieving, and invalidating cache
+    for functions using decorators. Supports caching with tags or prefixes and
+    integrates with the specified backend and key maker.
+    """
     def __init__(self):
+        """
+        Initializes the CacheManager with optional backend and key maker.
+        """
         self.backend: Optional[BaseBackend] = None
         self.key_maker: Optional[BaseKeyMaker] = None
 
     def init(self, *, backend: BaseBackend, key_maker: BaseKeyMaker) -> None:
+        """
+        Initializes the cache manager with the specified backend and key maker.
+
+        Args:
+            backend (BaseBackend): The caching backend to use.
+            key_maker (BaseKeyMaker): The key maker used for generating cache keys.
+        """
         self.backend = backend
         self.key_maker = key_maker
 
@@ -23,7 +38,20 @@ class CacheManager:
         tag: Optional[CacheTag] = None,
         ttl: int = 60,
     ) -> Callable[[F], F]:
-        """Decorator to cache data."""
+        """
+        Decorator to cache the result of a function call.
+
+        This decorator stores the result of the function in the cache if it's not
+        already cached, and retrieves it from the cache if it exists.
+
+        Args:
+            prefix (Optional[str]): The prefix for the cache key.
+            tag (Optional[CacheTag]): A cache tag to use in the key generation.
+            ttl (int): Time-to-live (TTL) for the cache, in seconds. Default is 60.
+
+        Returns:
+            Callable[[F], F]: The decorated function.
+        """
 
         def _cached(function: F) -> F:
             @wraps(function)
@@ -49,7 +77,17 @@ class CacheManager:
         return _cached
 
     def invalidate_by_prefix(self, prefix: str) -> Callable[[F], F]:
-        """Decorator to invalidate cache by prefix."""
+        """
+        Decorator to invalidate cache entries by prefix.
+
+        This decorator deletes all cache entries that start with the given prefix.
+
+        Args:
+            prefix (str): The prefix to use for invalidating cache entries.
+
+        Returns:
+            Callable[[F], F]: The decorated function.
+        """
 
         def _invalidate(function: F) -> F:
             @wraps(function)
@@ -65,7 +103,17 @@ class CacheManager:
         return _invalidate
 
     def invalidate_by_tag(self, tag: CacheTag) -> Callable[[F], F]:
-        """Decorator to invalidate cache by tag."""
+        """
+        Decorator to invalidate cache entries by tag.
+
+        This decorator deletes all cache entries associated with the given tag.
+
+        Args:
+            tag (CacheTag): The tag to use for invalidating cache entries.
+
+        Returns:
+            Callable[[F], F]: The decorated function.
+        """
 
         def _invalidate(function: F) -> F:
             @wraps(function)
@@ -81,11 +129,23 @@ class CacheManager:
         return _invalidate
 
     async def remove_by_tag(self, *, tag: CacheTag) -> None:
+        """
+        Remove all cache entries associated with a specific tag.
+
+        Args:
+            tag (CacheTag): The tag whose associated cache entries should be removed.
+        """
         if not self.backend:
             raise Exception("backend is None.")
         await self.backend.delete_startswith(value=tag.value)
 
     async def remove_by_prefix(self, *, prefix: str) -> None:
+        """
+        Remove all cache entries that start with the given prefix.
+
+        Args:
+            prefix (str): The prefix whose associated cache entries should be removed.
+        """
         if not self.backend:
             raise Exception("backend is None.")
         await self.backend.delete_startswith(value=prefix)
